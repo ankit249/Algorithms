8c0852ffabf7bd7de292bebd429e1aa1c448b7b0
Ankit Shah <shahankit@gmail.com>
2016-03-13 11:16:45 -0700

    BST in Rotated Array and First

---
+++ src/com/ds/sort/BinarySearchInRotatedArray.java
@@ -0,0 +1,37 @@
+package com.ds.sort;
+
+public class BinarySearchInRotatedArray {
+
+	private static int binarySearchRotatedArray(int[] a, int key) {
+		int lo = 0;
+		int hi = a.length - 1;
+
+		while (lo < hi) {
+			int mid = (lo + hi) / 2;
+			if (a[mid] == key)
+				return mid;
+			if (a[lo] < a[mid]) { // left portion is sorted half
+				if (key > a[lo] && key < a[mid]) {
+					hi = mid - 1;
+				} else {
+					lo = mid + 1;
+				}
+			} else if (a[hi] > a[mid]) { // right portion is sorted half
+				if (key > a[mid] && key < a[hi]) {
+					lo = mid + 1;
+				} else {
+					hi = mid - 1;
+				}
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int[] a = { 4, 5, 6, 7, 8, 1, 2, 3 };
+
+		System.out.println(binarySearchRotatedArray(a, 2));
+		System.out.println(binarySearchRotatedArray(a, 5));
+		System.out.println(binarySearchRotatedArray(a, 13));
+	}
+}
