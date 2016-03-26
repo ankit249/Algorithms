87a21e18b6124b67d731dd3491843be7c8be42d6
ankit249 <shahankit@gmail.com>
2016-03-25 17:01:01 -0700

    Unique characters within a string

---
+++ src/com/ds/string/StringUniqueChars.java
@@ -0,0 +1,47 @@
+package com.ds.string;
+
+public class StringUniqueChars {
+
+	// O(n) time complexity and O(n) space complexity
+	private static boolean isUniqueChars(String string) {
+		boolean[] b = new boolean[26];
+		for (int i = 0; i < string.length(); i++) {
+			if (!b['z' - string.charAt(i)]) { // or !b[string.charAt(i) - 'a']
+				b['z' - string.charAt(i)] = true;
+			} else {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+
+	// O(n) time complexity and o(1) space complexity
+	private static boolean isUniqueChars2(String string) {
+		int checker = 0;
+		for (int i = 0; i < string.length(); i++) {
+			int val = 'z' - string.charAt(i);
+			if ((checker & (1 << val)) > 0)
+				return false;
+			checker |= 1 << val;
+		}
+		return true;
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println("ankit: " + isUniqueChars("ankit"));
+		System.out.println("ankit: " + isUniqueChars2("ankit"));
+
+		System.out.println("aankit: " + isUniqueChars("aankit"));
+		System.out.println("aankit: " + isUniqueChars2("aankit"));
+
+		System.out.println("zyxw: " + isUniqueChars("zyxw"));
+		System.out.println("zyxw: " + isUniqueChars2("zyxw"));
+
+		System.out.println("zzyyzeex: " + isUniqueChars("zzyyzeex"));
+		System.out.println("zzyyzeex: " + isUniqueChars2("zzyyzeex"));
+
+	}
+}
