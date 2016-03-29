eea8aaef65fbb3a1a6a662307a628c0af6c50bed
ankit249 <shahankit@gmail.com>
2016-03-29 15:52:25 -0700

    Parenthesis

--- src/com/ds/recursion/Parenthesis.java
+++ src/com/ds/recursion/Parenthesis.java
@@ -1,8 +1,53 @@
 package com.ds.recursion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Parenthesis {
 
-	public static void printPar(int l, int r, char[] str, int count) {
+	public static List<String> generateParenthesis(int n) {
+		ArrayList<String> result = new ArrayList<String>();
+		dfs(result, "", n, n);
+		return result;
+	}
+
+	/*
+	 * left and right represents the remaining number of ( and ) that need to be added. When left > right, there are
+	 * more ")" placed than "(". Such cases are wrong and the method stops.
+	 */
+	public static void dfs(ArrayList<String> result, String s, int left, int right) {
+		if (left > right)
+			return;
+
+		if (left == 0 && right == 0) {
+			result.add(s);
+			return;
+		}
+
+		if (left > 0) {
+			dfs(result, s + "(", left - 1, right);
+		}
+
+		if (right > 0) {
+			dfs(result, s + ")", left, right - 1);
+		}
+	}
+
+	public static void main(String[] args) {
+		int num = 3;
+
+		List<String> list = generateParenthesis(num);
+		System.out.println(list);
+        /*
+		char[] str = new char[num * 2];
+		printPar(num, num, str, 0);
+		
+		*/
+
+	}
+	
+    /*
+ 	public static void printPar(int l, int r, char[] str, int count) {
 		if (l < 0 || r < l)
 			return; // invalid state
 		if (l == 0 && r == 0) {
@@ -20,11 +65,6 @@ public class Parenthesis {
 			}
 		}
 	}
+    */
 
-	public static void main(String[] args) {
-		int num = 3;
-
-		char[] str = new char[num * 2];
-		printPar(num, num, str, 0);
-	}
 }
