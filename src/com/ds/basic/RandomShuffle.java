8ebddb1d6e1d960f1d7684d0e599f034dac444f4
ankit249 <shahankit@gmail.com>
2016-03-30 11:44:31 -0700

    Random Shuffle, run the reverse loop from a.length - 1 to 1 and swap with rand(i)

--- src/com/ds/basic/RandomShuffle.java
+++ src/com/ds/basic/RandomShuffle.java
@@ -12,28 +12,34 @@ public class RandomShuffle {
 
 	
 	/**
-	 * random no from: 0 to 9 ---> swap a[0] with random
-	 * random no from: 1 to 9 ---> swap a[1] with random
-	 * random no from: 2 to 9 ---> swap a[2] with random
-	 * random no from: 3 to 9 ---> swap a[3] with random
-	 * random no from: 4 to 9 ---> swap a[4] with random
+	 * random no from: 5 to 0 ---> swap a[5] with random
+	 * random no from: 4 to 0 ---> swap a[4] with random
+	 * random no from: 3 to 0 ---> swap a[3] with random
+	 * random no from: 2 to 0 ---> swap a[2] with random
+	 * random no from: 1 to 0 ---> swap a[1] with random
 	 * ...
 	 * ...
 	 * 
 	 */
 	public static void shuffle(int[] a) {
 		Random r = new Random();
-		int n = a.length;
-		for (int i = 0; i < n; i++) {
-			int random = r.nextInt(n - i);
-			int tmp = a[random];
-			a[random] = a[i];
-			a[i] = tmp;
+		for (int i = a.length - 1; i > 0; i--) {
+			int random = r.nextInt(i);
+			swap(a, i, random);
 		}
+	}
+
+	private static void swap(int[] a, int i, int j) {
+		int tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
 	}
 
 	public static void main(String[] args) {
 		int[] a = { 6, 9, 2, 12, 34, 56, 43, 22 };
+
+		// JDK shuffle method
+		// Collections.shuffle(Arrays.asList(a));
 		System.out.println("original array: " + Arrays.toString(a));
 
 		for (int i = 0; i < 20; i++) {
