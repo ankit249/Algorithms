3325b7b1057f62e9ebdad4b1a031de8bd54cc5fa
ankit249 <shahankit@gmail.com>
2016-03-31 18:33:40 -0700

    optimizations

--- src/com/ds/sort/BinarySearchStringWithEmptyInputs.java
+++ src/com/ds/sort/BinarySearchStringWithEmptyInputs.java
@@ -4,29 +4,22 @@ public class BinarySearchStringWithEmptyInputs {
 
 	private static int binarySearch(String[] a, String key) {
 
-		int lo = (a[0].equals("") == true) ? moveIndexRight(a, 0) : 0;
-		int hi = (a[a.length - 1].equals("") == true) ? moveIndexLeft(a, a.length - 1) : a.length - 1;
-
-		if (lo == -1 || hi == -1)
-			return -1;
+		int lo = (a[0].equals("")) ? moveIndexRight(a, 0) : 0;
+		int hi = (a[a.length - 1].equals("")) ? moveIndexLeft(a, a.length - 1) : a.length - 1;
 
 		while (lo <= hi) {
 			int mid = (lo + hi) / 2;
-			mid = (a[mid].equals("") == true) ? moveIndexRight(a, mid) : mid;
+			mid = (a[mid].equals("")) ? moveIndexRight(a, mid) : mid;
 
 			if (key.equals(a[mid]))
 				return mid;
 			if (key.compareTo(a[mid]) < 0) {
 				hi = mid - 1;
-				hi = (a[hi].equals("") == true) ? moveIndexLeft(a, hi) : hi;
+				hi = (a[hi].equals("")) ? moveIndexLeft(a, hi) : hi;
 			} else {
 				lo = mid + 1;
-				lo = (a[lo].equals("") == true) ? moveIndexRight(a, lo) : lo;
+				lo = (a[lo].equals("")) ? moveIndexRight(a, lo) : lo;
 			}
-
-			if (lo == -1 || hi == -1)
-				return -1;
-
 		}
 
 		return -1;
@@ -34,35 +27,31 @@ public class BinarySearchStringWithEmptyInputs {
 
 	private static int moveIndexRight(String[] a, int currentIndex) {
 
-		int index = -1;
-
 		// assuming current index is blank
 		while (currentIndex < a.length && a[currentIndex].equals("")) {
 			currentIndex++;
-			index = currentIndex;
 		}
 
-		return index;
+		return currentIndex;
 	}
 
 	private static int moveIndexLeft(String[] a, int currentIndex) {
-
-		int index = -1;
-
 		// assuming current index is blank
 		while (currentIndex >= 0 && a[currentIndex].equals("")) {
 			currentIndex--;
-			index = currentIndex;
 		}
 
-		return index;
+		return currentIndex;
 	}
 
 	public static void main(String[] args) {
 
 		// string array is sorted but there might be blanks in them
 		String[] input = { "", "", "a", "b", "", "c", "", "d", "", "e", "", "f", "", "", "" };
-		System.out.println(binarySearch(input, "f"));
+		System.out.println(binarySearch(input, "g"));
+
+		String[] input2 = { "", "", "", "", "" };
+		System.out.println(binarySearch(input2, "g"));
 	}
 
 }
