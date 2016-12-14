23ee30d0a76dec39882743e610217b88d1624bcd
ankit_shah2 <ankit_shah2@m104006dmg8wp.symc.symantec.com>
2016-12-13 16:01:46 -0800

    tail recursion for fibonacci.

--- src/com/ds/recursion/Fibonacci.java
+++ src/com/ds/recursion/Fibonacci.java
@@ -4,8 +4,10 @@ public class Fibonacci {
 
 	// O(2^n) exponential time complexity with O(1) space
 	public static int fib(int n) {
-		if (n == 0) return 0;
-		if (n == 1) return 1;
+		if (n == 0)
+			return 0;
+		if (n == 1)
+			return 1;
 		return fib(n - 1) + fib(n - 2);
 	}
 
@@ -22,9 +24,31 @@ public class Fibonacci {
 		return c;
 	}
 
+	// tail recursion
+	public static int tailRecursive(int n) {
+		if (n <= 2) {
+			return 1;
+		}
+		return tailRecursiveAux(0, 1, n);
+	}
+
+	private static int tailRecursiveAux(int a, int b, int n) {
+		if (n <= 0) {
+			return a;
+		}
+		return tailRecursiveAux(b, a + b, n - 1);
+	}
+
 	public static void main(String[] args) {
 		for (int i = 0; i < 20; i++) {
 			System.out.print(fib_iterative(i) + " ");
 		}
+
+		// Tail recursion
+		System.out.println();
+		System.out.println("Using Tail Recursion....");
+		System.out.println(tailRecursive(10));
+
 	}
+
 }
