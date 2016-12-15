b9e6bdd7d116e6f1eb7b2ea8347b4f026f2f1e5d
ankit_shah2 <ankit_shah2@m104006dmg8wp.symc.symantec.com>
2016-12-15 12:14:23 -0800

    Modify indices for fibonacci.

--- src/com/ds/recursion/Fibonacci.java
+++ src/com/ds/recursion/Fibonacci.java
@@ -1,39 +1,33 @@
 package com.ds.recursion;
 
+// 0, 1, 2, 3, 4, 5, 6
+// 0, 1, 1, 2, 3, 5, 8
 public class Fibonacci {
 
 	// O(2^n) exponential time complexity with O(1) space
 	public static int fib(int n) {
-		if (n == 0)
-			return 0;
-		if (n == 1)
-			return 1;
+		if (n == 0) return 0;
+		if (n == 1 || n == 2) return 1;
 		return fib(n - 1) + fib(n - 2);
 	}
 
 	// O(1) space and O(n) time complexity
-	
-
-	
-	// 1,1,2,3,5
-	// 1,2,3,4,5 (fib 5 is 5)
 	public static int fib_iterative(int n) {
 		int a = 0;
 		int b = 1;
 		int c = 0;
 		for (int i = 0; i < n; i++) {
-			c = a + b;
 			a = b;
 			b = c;
+			c = a + b;
 		}
 		return c;
 	}
 
 	// tail recursion
 	public static int tailRecursive(int n) {
-		if (n <= 2) {
-			return 1;
-		}
+		if(n == 0) return 0;
+		if(n == 1 || n == 2) return 1;
 		return fibtail(0, 1, n);
 	}
 
@@ -45,14 +39,14 @@ public class Fibonacci {
 	}
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i <= 5; i++) {
 			System.out.print(fib_iterative(i) + " ");
 		}
 
 		// Tail recursion
 		System.out.println();
 		System.out.println("Using Tail Recursion....");
-		System.out.println(tailRecursive(10));
+		System.out.println(tailRecursive(5));
 
 	}
 
