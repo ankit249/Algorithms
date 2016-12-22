94c716bcbe8b23798c96a416d56158e631e170be
ankit_shah2 <ankit_shah2@m104006dmg8wp.symc.symantec.com>
2016-12-21 16:25:13 -0800

    with double x

--- src/com/ds/recursion/Exponential.java
+++ src/com/ds/recursion/Exponential.java
@@ -2,15 +2,37 @@ package com.ds.recursion;
 
 public class Exponential {
 
-	public static int exp(int n, int t) {
-		if(n <= 0 || t <= 0 ) return 1;
-		if(t == 1) return n;
-		return(n * exp(n, t-1));
+	public static int pow(int x, int n) {
+		if (x == 0 || n == 0)
+			return 1;
+		if (n > 0)
+			return (x * pow(x, n - 1));
+		else
+			return (1 / x * pow(x, n + 1));
+
 	}
-	
+
 	public static void main(String[] args) {
 		for (int i = 0; i < 32; i++) {
-			System.out.print(exp(2, i) + " ");
+			System.out.print(pow(2, i) + " ");
 		}
+
+		System.out.println("");
+		System.out.println("Double solution");
+
+		System.out.println(dpow(2, 3));
+	}
+
+	static double dpow(double x, int n) {
+		if (n == 0)
+			return 1;
+		double half = dpow(x, n / 2);
+
+		if (n % 2 == 0)
+			return half * half;
+		else if (n > 0)
+			return half * half * x;
+		else
+			return half * half / x;
 	}
 }
