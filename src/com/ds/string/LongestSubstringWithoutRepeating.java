d68854d48ed28fcf76656e6a2705871b3f9e1427
ankit_shah2 <shahankit@gmail.com>
2016-12-23 10:07:54 -0800

    change the output and pointer name

--- src/com/ds/string/LongestSubstringWithoutRepeating.java
+++ src/com/ds/string/LongestSubstringWithoutRepeating.java
@@ -3,10 +3,7 @@ package com.ds.string;
 import java.util.HashMap;
 import java.util.Map;
 
-// works for all the cases in the main program
 public class LongestSubstringWithoutRepeating {
-
-
 	private static String longestSubstringWithoutRepeatation(String string) {
 		if (string == null || string == "") {
 			return null;
@@ -15,49 +12,43 @@ public class LongestSubstringWithoutRepeating {
 		if (string.length() == 1) {
 			return string;
 		}
+
 		Map<Character, Integer> map = new HashMap<Character, Integer>();
-
 		int max = Integer.MIN_VALUE;
+		int index = 0;
 
-		int hi = 0;
 		for (int i = 0; i < string.length() - 1; i++) {
-
 			if (!map.containsKey(string.charAt(i))) {
 				map.put(string.charAt(i), i);
 			} else {
-
 				// if you only need length of the largestsubstring
 				// max = Math.max(max, map.size());
-
 				if (map.size() > max) {
-					hi = i;
+					index = i;
 					max = map.size();
 				}
 				i = map.get(string.charAt(i)); // loop starts from map.get plus one after for loop
 				map.clear();
 			}
 		}
-
 		// if you only need length of the largestsubstring
 		// max = Math.max(max, map.size());
 		// return max
-
 		if (map.size() > max) {
-			hi = string.length() - 1;
+			index = string.length() - 1;
 			max = map.size();
 		}
-
-		return string.substring(hi - max, hi);
+		return string.substring(index - max, index);
 	}
 
 	public static void main(String[] args) {
 
-		System.out.println(longestSubstringWithoutRepeatation("abcabcbb"));
-		System.out.println(longestSubstringWithoutRepeatation("xyzxxabcyzxyz"));
-		System.out.println(longestSubstringWithoutRepeatation("bbbbbbb"));
-		System.out.println(longestSubstringWithoutRepeatation("abcabcbb"));
-		System.out.println(longestSubstringWithoutRepeatation(""));
-		System.out.println(longestSubstringWithoutRepeatation(null));
+		System.out.println("abcabcbb: " + longestSubstringWithoutRepeatation("abcabcbb"));
+		System.out.println("xyzxxabcyzxyz: " + longestSubstringWithoutRepeatation("xyzxxabcyzxyz"));
+		System.out.println("bbbbbbb: " + longestSubstringWithoutRepeatation("bbbbbbb"));
+		System.out.println("abcabcbb: " + longestSubstringWithoutRepeatation("abcabcbb"));
+		System.out.println(" '':" + longestSubstringWithoutRepeatation(""));
+		System.out.println("null: " + longestSubstringWithoutRepeatation(null));
 
 		// System.out.println("acbdxx".indexOf('b'));
 
