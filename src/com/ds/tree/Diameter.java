2c4b19526398f9c53d211c14ae9cde025457e29e
ankit_shah2 <shahankit@gmail.com>
2016-12-23 09:14:24 -0800

    Corrected both diameter and MaxPathSum

--- src/com/ds/tree/Diameter.java
+++ src/com/ds/tree/Diameter.java
@@ -6,10 +6,31 @@ class Height {
 
 public class Diameter {
 
+	public static int diameterOfBinaryTree(TreeNode node) {
+		if (node == null) {
+			return 0;
+		}
+
+		int leftHeight = maxdepth(node.left);
+		int rightHeight = maxdepth(node.right);
+
+		int leftDiameter = diameterOfBinaryTree(node.left);
+		int rightDiameter = diameterOfBinaryTree(node.right);
+
+		return Math.max(leftHeight + rightHeight + 1, Math.max(leftDiameter, rightDiameter));
+	}
+
+	private static int maxdepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		return 1 + Math.max(maxdepth(root.left), maxdepth(root.right));
+	}
 
 	/*
 	 * define height =0 globally and call diameter(root,height) from main
 	 */
+	// This method result is incorrect
 	int diameter(TreeNode root, Height currentHeight) {
 		if (root == null) {
 			currentHeight.h = 0;
@@ -41,24 +62,41 @@ public class Diameter {
 		return Math.max(leftHeight.h + rightHeight.h + 1, Math.max(ldiameter, rdiameter));
 	}
 
-
 	public static void main(String args[]) {
 		/* creating a binary tree and entering the nodes */
-		TreeNode root = new TreeNode(1);
-
-		root.left = new TreeNode(2);
-		root.right = new TreeNode(3);
-		root.left.left = new TreeNode(4);
-		root.left.right = new TreeNode(5);
-		root.left.right.left = new TreeNode(20);
-		root.left.right.right = new TreeNode(70);
-
+		
+		
+		BinarySearchTree bst = new BinarySearchTree();
+		TreeNode root = new TreeNode(40);
+		bst.insert(root, 20);
+		bst.insert(root, 10);
+		bst.insert(root, 1);
+		bst.insert(root, 2);
+		bst.insert(root, 3);
+		bst.insert(root, 30);
+		bst.insert(root, 35);
+		bst.insert(root, 32);
+		bst.insert(root, 33);
+		bst.insert(root, 50);
+		
+		
+		/*
+		BinarySearchTree bst = new BinarySearchTree();
+		TreeNode root = new TreeNode(3);
+		bst.insert(root, 1);
+		bst.insert(root, -1);
+		bst.insert(root, -2);
+		bst.insert(root, 2);
+		bst.insert(root, 4);
+		*/
 		// original tree
 		BTreePrinter.printNode(root);
 
 		Diameter d = new Diameter();
 		Height height = new Height();
 
-		System.out.println("The diameter of given binary tree is : " + d.diameter(root, height));
+		System.out.println("The diameter of given binary tree is (Method 1) : " + d.diameter(root, height));
+		System.out.println("The diameter of given binary tree is (Method 2: Correct): " + diameterOfBinaryTree(root));
+
 	}
 }
--- src/com/ds/tree/MaxPathSum.java
+++ src/com/ds/tree/MaxPathSum.java
@@ -2,29 +2,31 @@ package com.ds.tree;
 
 public class MaxPathSum {
 
-	public int maxPathSum(TreeNode root) {
-		int[] max = new int[1];
-		max[0] = Integer.MIN_VALUE;
-		calculateSum(root, max);
-		return max[0];
-	}
+	private static int maxPathSum(TreeNode node, int sum) {
 
-	public int calculateSum(TreeNode root, int[] max) {
-		if (root == null)
+		if (node == null)
 			return 0;
 
-		int left = calculateSum(root.left, max);
-		int right = calculateSum(root.right, max);
+		int leftSum = maxPathSum(node.left, sum);
+		int rightSum = maxPathSum(node.right, sum);
 
-		int current = Math.max(root.data, Math.max(root.data + left, root.data + right));
-		max[0] = Math.max(max[0], Math.max(current, left + root.data + right));
+		int max = Math.max(node.data, Math.max(node.data + leftSum, node.data + rightSum));
+		int result = Math.max(sum, Math.max(max, node.data + leftSum + rightSum));
 
-		return current;
+		return result;
+
 	}
 
 	public static void main(String[] args) {
 
 		BinarySearchTree bst = new BinarySearchTree();
+		TreeNode root = new TreeNode(3);
+		bst.insert(root, 1);
+		bst.insert(root, -1);
+		bst.insert(root, -2);
+		bst.insert(root, 2);
+		bst.insert(root, 4);
+/*
 		TreeNode root = new TreeNode(7);
 		bst.insert(root, 5);
 		bst.insert(root, 21);
@@ -38,11 +40,11 @@ public class MaxPathSum {
 		bst.insert(root, 24);
 		bst.insert(root, 16);
 		bst.insert(root, 18);
-
+*/
 		BTreePrinter.printNode(root);
 
 		MaxPathSum mps = new MaxPathSum();
-		System.out.println(mps.maxPathSum(root));
+		System.out.println(maxPathSum(root, 0));
 
 		System.out.print(
 				"The Sum (might not go thru root just like this example, diameter is no. of nodes in this path): ");
