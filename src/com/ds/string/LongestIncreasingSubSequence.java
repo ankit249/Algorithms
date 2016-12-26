0afef0f895a3101bbaa0564c4a25b3864f87491b
ankit_shah2 <shahankit@gmail.com>
2016-12-26 10:14:58 -0800

    video link added

--- src/com/ds/string/LongestIncreasingSubSequence.java
+++ src/com/ds/string/LongestIncreasingSubSequence.java
@@ -1,5 +1,6 @@
 package com.ds.string;
 
+//https://www.youtube.com/watch?v=CE2b_-XfVDk
 public class LongestIncreasingSubSequence {
 
 	public static void printLIS(int[] a) {
@@ -16,15 +17,11 @@ public class LongestIncreasingSubSequence {
 
 		for (int i = 1; i < a.length; i++) {
 			for (int j = 0; j < i; j++) {
-				if (a[i] > a[j] && sizes[i] < sizes[j] + 1) {
+				if (a[j] < a[i] && sizes[i] < sizes[j] + 1) {
 					sizes[i] = sizes[j] + 1;
 					paths[i] = paths[j] + a[i] + " ";
-					if (max < sizes[i]) {
-						max = sizes[i];
-					}
+					max = Math.max(max, sizes[i]);
 				}
-
-
 			}
 		}
 
@@ -37,7 +34,7 @@ public class LongestIncreasingSubSequence {
 	}
 
 	public static void main(String[] args) {
-		int[] a = { 7, 6, 1, 2, 4, 5, 6, 7, -3, 1, 2, 4, 5, 6 };
+		int[] a = { 7, 6, 1, 2, 5, 6, 7, -3, 1, 2, 4, 5, 6 };
 
 		printLIS(a);
 	}
