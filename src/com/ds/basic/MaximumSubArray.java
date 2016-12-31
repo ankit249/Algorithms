a1e37afd8f01777ee59d8a2f484f1f7962338898
ankit_shah2 <shahankit@gmail.com>
2016-12-31 10:21:18 -0800

    Max subarray

--- src/com/ds/basic/MaximumSubArray.java
+++ src/com/ds/basic/MaximumSubArray.java
@@ -4,12 +4,15 @@ import java.util.Arrays;
 
 public class MaximumSubArray {
 	public static void main(String[] args) {
-		// int[] a = { -1, -1, -1, -1, -1, -1, -1 };
+		// int[] a = { -2, -1, -3, -4, -5, -6, -7 };
 		int[] a = { -1, 3, -5, 4, 6, -1, 2, -7, 13, -3 };
-		// int[] a={-6,-2,-3,-4,-1,-5,-5};
+		// int[] a = { -6, -2, -3, -4, -1, -5, -5 };
 		System.out.println(Arrays.toString(a));
-
+		// find all the indices and everything
 		findMaxSubArray(a);
+
+		// find just maximum
+		System.out.println(findMaxSum(a));
 	}
 
 	public static void findMaxSubArray(int[] a) {
@@ -20,6 +23,7 @@ public class MaximumSubArray {
 
 		int currentsum = 0;
 		int indexUntilNow = 0;
+
 
 		for (int i = 0; i < a.length; i++) {
 
@@ -43,5 +47,17 @@ public class MaximumSubArray {
 		System.arraycopy(a, lo, b, 0, lengthOfNewArray);
 		System.out.println("Maximum SubArray   : " + Arrays.toString(b));
 
+	}
+
+	// 0(1) space, don't need DP here.
+	private static int findMaxSum(int[] a) {
+		int max = a[0];
+		int currentsum = a[0];
+
+		for (int i = 1; i < a.length; i++) {
+			currentsum = Math.max(currentsum + a[i], a[i]);
+			max = Math.max(max, currentsum);
+		}
+		return max;
 	}
 }
