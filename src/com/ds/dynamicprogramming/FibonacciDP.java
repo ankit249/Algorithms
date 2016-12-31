8b12703f0cae2b106867fac98db0f4ffc178e802
ankit_shah2 <shahankit@gmail.com>
2016-12-31 12:23:37 -0800

    Fib Dynamic programming

--- src/com/ds/dynamicprogramming/FibonacciDP.java
+++ src/com/ds/dynamicprogramming/FibonacciDP.java
@@ -3,18 +3,27 @@ package com.ds.dynamicprogramming;
 // O(n) space and O(n) time complexity
 public class FibonacciDP {
 
-	private static int fib(int n) {
-
+	private static int fibDP(int n) {
+		if(n == 0) return 0;
+		if(n == 1 || n == 2) return 1;
+		
 		int[] dp = new int[n + 1];
 		dp[0] = 1;
 		dp[1] = 1;
-		for (int i = 2; i < n + 1; i++) {
+		dp[2] = 1;
+		for (int i = 3; i < n + 1; i++) {
 			dp[i] = dp[i - 1] + dp[i - 2];
 		}
 		return dp[n];
 	}
 
 	public static void main(String[] args) {
-		System.out.println(fib(5));
+		// Dynamic Programming
+		System.out.println();
+		System.out.println("Fibonacci with DP....");
+		for (int i = 0; i <= 6; i++) {
+			System.out.print(fibDP(i) + " ");
+		}
+
 	}
 }
