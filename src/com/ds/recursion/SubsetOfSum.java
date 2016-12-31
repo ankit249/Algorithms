d4dedc90c1ef7bce582d7af555c8bae93141bef1
ankit_shah2 <shahankit@gmail.com>
2016-12-30 17:16:51 -0800

    Subset of sum

--- src/com/ds/recursion/SubsetOfSum.java
+++ src/com/ds/recursion/SubsetOfSum.java
@@ -4,32 +4,31 @@ package com.ds.recursion;
 // time complexity O(2n)   <-- Exponential
 public class SubsetOfSum {
 
-	public static void subsetofsum(int start, int[] a, boolean[] barray, int target, int currentsum) {
+	public static void subsetofsum(int[] a, int index, boolean[] barray, int target, int currentsum) {
 
 		if (currentsum == target) {
-			System.out.print("Subset (");
 			for (int i = 0; i < barray.length; i++) {
 				if (barray[i]) {
 					System.out.print(a[i] + " ");
 				}
 			}
-			System.out.println(")");
+			System.out.println();
 			return;
 		}
 
-		if (start >= a.length) {
+		if (index >= a.length) {
 			return;
 		}
 
 		// select the start index in the current sum
-		barray[start] = true;
-		currentsum = currentsum + a[start];
-		subsetofsum(start + 1, a, barray, target, currentsum);
+		barray[index] = true;
+		currentsum = currentsum + a[index];
+		subsetofsum(a, index + 1, barray, target, currentsum);
 
 		// don't select the start index in the current sum
-		barray[start] = false;
-		currentsum = currentsum - a[start];
-		subsetofsum(start + 1, a, barray, target, currentsum);
+		barray[index] = false;
+		currentsum = currentsum - a[index];
+		subsetofsum(a, index + 1, barray, target, currentsum);
 
 	}
 
@@ -38,7 +37,7 @@ public class SubsetOfSum {
 		int target = 8;
 		boolean[] barray = new boolean[a.length];
 
-		subsetofsum(0, a, barray, target, 0);
+		subsetofsum(a, 0, barray, target, 0);
 
 	}
 }
