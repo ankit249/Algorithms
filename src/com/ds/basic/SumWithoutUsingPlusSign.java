07c7cda8f25ac4f74c426279eedeeeccecb09794
ankit_shah2 <shahankit@gmail.com>
2016-12-31 16:06:20 -0800

    sum with bits without plus sign.

--- src/com/ds/basic/SumWithoutUsingPlusSign.java
+++ src/com/ds/basic/SumWithoutUsingPlusSign.java
@@ -15,10 +15,31 @@ public class SumWithoutUsingPlusSign {
 		return a;
 	}
 
+	public static int getsum(int a, int b) {
+		int c = 0;
+		while (b != 0) {
+			// System.out.println("" + a + "; " + b + "; " + c);
+			c = a & b;
+			a = a ^ b;
+			b = c << 1;
+		}
+
+		return a;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(sum(1, 3));
 		System.out.println(sum(1, -3));
 		System.out.println(sum(-1, 3));
 		System.out.println(sum(-1, -3));
+		
+		System.out.println(getsum(5, 3));
+		System.out.println(getsum(1, -3));
+		System.out.println(getsum(-1, 3));
+		System.out.println(getsum(-1, -3));
+
+		for (int i = 1; i < 6; i++) {
+			System.out.println(i << 1);
+		}
 	}
 }
