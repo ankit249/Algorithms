9ed771b673148c2dbba6e1a48099648a9613e257
ankit_shah2 <shahankit@gmail.com>
2017-01-04 17:45:10 -0800

    Spiral order matrix

--- src/com/ds/matrix/SpiralOrder.java
+++ src/com/ds/matrix/SpiralOrder.java
@@ -6,13 +6,8 @@ public class SpiralOrder {
 
 	public static void main(String[] args) {
 
-		int[][] matrix = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
-
-		// System.out.println(matrix.length);
-
-		// System.out.println(matrix[0].length);
-
-		// System.out.println(matrix[1][0]);
+		// int[][] matrix = { { 1, 2, 3, 4 }, { 12, 13, 14, 5 }, { 11, 16, 15, 6 }, { 10, 9, 8, 7 } };
+		int[][] matrix = { { 1, 2, 3, 4 }, { 10, 11, 12, 5 }, { 9, 8, 7, 6 } };
 
 		ArrayList<Integer> list = spiralOrder(matrix);
 		System.out.println(list);
@@ -26,22 +21,22 @@ public class SpiralOrder {
 		if (matrix == null || matrix.length == 0)
 			return result;
 
-		int row = matrix.length;
-		int col = matrix[0].length;
+		int m = matrix.length;
+		int n = matrix[0].length;
 
 		int x = 0;
 		int y = 0;
 
-		while (row > 0 && col > 0) {
+		while (m > 0 && n > 0) {
 
 			// if one row/column left, no circle can be formed
-			if (row == 1) {
-				for (int i = 0; i < col; i++) {
+			if (m == 1) {
+				for (int i = 0; i < n; i++) {
 					result.add(matrix[x][y++]);
 				}
 				break;
-			} else if (col == 1) {
-				for (int i = 0; i < row; i++) {
+			} else if (n == 1) {
+				for (int i = 0; i < m; i++) {
 					result.add(matrix[x++][y]);
 				}
 				break;
@@ -50,29 +45,29 @@ public class SpiralOrder {
 			// below, process a circle
 
 			// top - move right
-			for (int i = 0; i < col - 1; i++) {
+			for (int i = 0; i < n - 1; i++) {
 				result.add(matrix[x][y++]);
 			}
 
 			// right - move down
-			for (int i = 0; i < row - 1; i++) {
+			for (int i = 0; i < m - 1; i++) {
 				result.add(matrix[x++][y]);
 			}
 
 			// bottom - move left
-			for (int i = 0; i < col - 1; i++) {
+			for (int i = 0; i < n - 1; i++) {
 				result.add(matrix[x][y--]);
 			}
 
 			// left - move up
-			for (int i = 0; i < row - 1; i++) {
+			for (int i = 0; i < m - 1; i++) {
 				result.add(matrix[x--][y]);
 			}
 
 			x++;
 			y++;
-			row = row - 2;
-			col = col - 2;
+			m = m - 2;
+			n = n - 2;
 		}
 
 		return result;
