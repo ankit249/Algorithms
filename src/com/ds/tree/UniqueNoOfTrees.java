1f7106877ad8566c2071763a72e358e26e135ba8
ankit_shah2 <shahankit@gmail.com>
2017-01-07 14:19:42 -0800

    Unique binary tress

--- src/com/ds/tree/UniqueNoOfTrees.java
+++ src/com/ds/tree/UniqueNoOfTrees.java
@@ -9,8 +9,8 @@ public class UniqueNoOfTrees {
 		dp[0] = 1;
 		dp[1] = 1;
 
-		for (int i = 2; i <= n; i++) {
-			for (int j = 0; j <= i - 1; j++) {
+		for (int i = 2; i < n + 1; i++) {
+			for (int j = 0; j < i; j++) {
 				dp[i] = dp[i] + dp[j] * dp[i - j - 1];
 			}
 		}
