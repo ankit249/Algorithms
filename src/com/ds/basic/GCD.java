3752c5043c5318e6504f5146965b8a736661c075
ankit_shah2 <shahankit@gmail.com>
2017-01-09 09:58:38 -0800

    Greatest common divisor

--- src/com/ds/basic/GCD.java
+++ src/com/ds/basic/GCD.java
@@ -3,14 +3,16 @@ package com.ds.basic;
 public class GCD {
 
 	public static int gcd(int p, int q) {
-		if (p == 0 || q == 0)
-			return p + q; // base case
-		return gcd(q, p % q);
+		if (q == 0) return p;
+		int r = p % q;
+		return gcd(q, r);
 	}
 
 
 	public static void main(String[] args) {
-		System.out.println(gcd(4, 2));
+		System.out.println(gcd(15, 27));
+		System.out.println(gcd(3, 4));
+		System.out.println(gcd(144, 224));
 
 		System.out.println("3 % 4 = " + 3 % 4);
 		System.out.println("4 % 3 = " + 4 % 3);
