1a16b2c89be7461fa2047bcfc0373d956c1609c2
ankit_shah2 <shahankit@gmail.com>
2017-01-09 11:09:45 -0800

    Change the pointer name to avoid confusion

--- src/com/ds/basic/PeekingIterator.java
+++ src/com/ds/basic/PeekingIterator.java
@@ -5,34 +5,34 @@ import java.util.Iterator;
 import java.util.List;
 
 public class PeekingIterator implements Iterator<Integer> {
-	private Integer next; // cache the next peek
+	private Integer peek; // cache the next peek
 	private Iterator<Integer> iter;
 
 	public PeekingIterator(Iterator<Integer> iterator) {
 		// initialize any member here.
 		iter = iterator;
 		if (iter.hasNext()) {
-			next = iter.next();
+			peek = iter.next();
 		}
 	}
 
 	// Returns the next element in the iteration without advancing the iterator.
 	public Integer peek() {
-		return next;
+		return peek;
 	}
 
 	// hasNext() and next() should behave the same as in the Iterator interface.
 	// Override them if needed.
 	@Override
 	public Integer next() {
-		Integer ret = next;
-		next = iter.hasNext() ? iter.next() : null;
+		Integer ret = peek;
+		peek = iter.hasNext() ? iter.next() : null;
 		return ret;
 	}
 
 	@Override
 	public boolean hasNext() {
-		return next != null;
+		return peek != null;
 	}
 
 	public static void main(String[] args) {
