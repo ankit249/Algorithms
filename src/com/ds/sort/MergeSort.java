f80a33a1aafd86b555c6812f60ec1b3010f6ca5d
ankit-shah2 <ankitshah@apple.com>
2020-12-25 14:57:42 -0800

    space adjustment

--- src/com/ds/sort/MergeSort.java
+++ src/com/ds/sort/MergeSort.java
@@ -22,8 +22,8 @@ public class MergeSort {
 	/*
     merge two sorted halves 3456 and 1278
 
-	auxilliary array                        main array
-	a										a
+	auxilliary array                       main array
+	a                                      a
 
 	lo      mid          hi                k
 	3  4  5  6  1  2  7  8                 3  4  5  6  1  2  7  8
@@ -33,11 +33,11 @@ public class MergeSort {
 	3  4  5  6  1  2  7  8                 1  4  5  6  1  2  7  8
 	i              j
 
-	lo      mid          hi					     k
+	lo      mid          hi                      k
 	3  4  5  6  1  2  7  8                 1  2  5  6  1  2  7  8
-	i				  j
+    i				  j
 
-	lo      mid          hi					        k
+	lo      mid          hi                         k
 	3  4  5  6  1  2  7  8                 1  2  3  6  1  2  7  8
        i			  j
 
