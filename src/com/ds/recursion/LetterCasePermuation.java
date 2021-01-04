daece2879163751570df8aead5f6435d54578f0d
ankit-shah2 <ankitshah@apple.com>
2021-01-04 13:50:20 -0800

    duplicate subset

--- src/com/ds/recursion/LetterCasePermuation.java
+++ src/com/ds/recursion/LetterCasePermuation.java
@@ -31,13 +31,13 @@ TC: O(2^k) where k are number of characters in the string with length n
 
 public class LetterCasePermuation {
 
-    private static List<String> letterCasePermuation(String input) {
+    private static List<String> letterCasePermuation(char[] input) {
         var result = new ArrayList<String>();
-        helper(input.toCharArray(), 0, new char[input.length()], result);
+        helper(input, new char[input.length], 0, result);
         return result;
     }
 
-    private static void helper(char[] input, int pos, char[] slate, List<String> result) {
+    private static void helper(char[] input, char[] slate, int pos, List<String> result) {
         // no backtracking needed
 
         // base case
@@ -49,18 +49,18 @@ public class LetterCasePermuation {
         // recursion call
         if(Character.isLetter(input[pos])) {  // checks both uppercase or lowercase
             slate[pos] = Character.toLowerCase(input[pos]);
-            helper(input, pos + 1, slate, result);
+            helper(input, slate, pos + 1, result);
             slate[pos] = Character.toUpperCase(input[pos]);
-            helper(input, pos + 1, slate, result);
+            helper(input, slate, pos + 1, result);
         } else {
             slate[pos] = input[pos];
-            helper(input, pos + 1, slate, result);
+            helper(input, slate, pos + 1, result);
         }
     }
 
     public static void main(String[] args) {
         String input = "a12b3";
-        List<String> results = letterCasePermuation(input);
+        List<String> results = letterCasePermuation(input.toCharArray());
         System.out.println(results);
     }
 }
--- src/com/ds/recursion/Subsets.java
+++ src/com/ds/recursion/Subsets.java
@@ -1,12 +1,14 @@
 package com.ds.recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Given a list of distinct objects (e.g. numbers) return all substes
  *
  * input: [1, 2, 3]
+ * output: [[],[1,2,3],[1,2],[1,3],[1],[2,3],[2],[3]]
  *
  *                             []
  *                 /                       \
@@ -31,19 +33,19 @@ public class Subsets {
 		return results;
 	}
 
-	private static void helper(List<Integer> input, int index, List<Integer> slate, List<List<Integer>> results) {
+	private static void helper(List<Integer> input, int pos, List<Integer> slate, List<List<Integer>> results) {
 		// (1) basecase
-		if(index >= input.size()) {
+		if(pos >= input.size()) {
 			results.add(new ArrayList<Integer>(slate));
 			return;
 		}
 
 		// (2) recursion
-		slate.add(input.get(index));
-		helper(input, index + 1, slate, results);
+		slate.add(input.get(pos));
+		helper(input, pos + 1, slate, results);
 		slate.remove(slate.size() - 1);   //  (3) unmodify
 											    //      remove is needed, if in doubt in any recursion always use remove
-		helper(input, index + 1, slate, results);
+		helper(input, pos + 1, slate, results);
 	}
 
 
@@ -78,43 +80,42 @@ public class Subsets {
  *         [1,2]     [1]              [2]       []      handle 2
  *          / \      /   \            / \       / \
  *    [1,2,2] [1,2] [1,2] [1]    [2,2]   [2]  [2]  []    handle 3
- *                   ---                     ---
+ *                  ---                       ---
  *   you can remove dups, but it'be nicer to not add dup, here [1,2] and [2] has dups
  *
- *   Answer is sort the array and don't add the underlined set, which is the
+ *   Answer is sort the array and don't add the underlined set, don't proceed to the next number, but proceed to a non duplicate number (pos + x)
  *
  */
 
 	public static List<List<Integer>> getSubsetsDups(List<Integer> input) {
 		var results = new ArrayList<List<Integer>>();
+		Arrays.sort(input.toArray());
 		helperDups(input, 0, new ArrayList<Integer>(), results);
 		return results;
 	}
 
-	private static void helperDups(List<Integer> input, int index, List<Integer> slate, List<List<Integer>> results) {
+	private static void helperDups(List<Integer> input, int pos, List<Integer> slate, List<List<Integer>> results) {
 		// (1) basecase
-		if(index >= input.size()) {
+		if(pos >= input.size()) {
 			results.add(new ArrayList<Integer>(slate));
 			return;
 		}
 
-		// (2) recursion
-		slate.add(input.get(index));
+		//(2) recursion
+		slate.add(input.get(pos));
+		helperDups(input, pos + 1, slate, results);
+		slate.remove(slate.size() - 1);   //  (3) unmodify
+												//      remove is needed, if in doubt in any recursion always use remove
 
-		// don't add this for the duplicate case
-		helperDups(input, index + 1, slate, results);
-		slate.remove(slate.size() - 1);
-
-		//TODO
-		if(index + 1 < input.size() && input.get(index) != input.get(index + 1)) {
-			helperDups(input, index + 1, slate, results);
-		}
+		int x = nextdiff(input, pos);
+		helperDups(input, pos + x, slate, results);
 	}
 
-	// TODO
-	private static int nextdiff(List<Integer> input, int index) {
-		for(int i = index + 1; i < input.size(); i++) {
-
+	private static int nextdiff(List<Integer> input, int pos) {
+		for(int i = pos + 1; i < input.size(); i++) {
+			if(input.get(pos) != input.get(i)) {
+				return  i;
+			}
 		}
 		return  input.size();
 	}
